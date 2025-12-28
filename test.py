ea5f96bf23b7bbee8a8f0b661e343eb1da5f559e
Yasasi-sandupama <yasasiugc@gmail.com>
2025-12-28 21:37:43 +0530

    feat: change python code

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-print("Hello")
-print("World")
-print(2 + 3)
+print("Python is working!")
+print("This is a test.")
+
+# Simple math
+x = 10
+y = 5
+print(f"{x} + {y} = {x + y}")
+print(f"{x} * {y} = {x * y}")
+
+print("Done.")
